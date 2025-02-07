243ee5038137ab7284be3f1d0c8feb73e9b92729
Wei-Chen Li <williamrob104@gmail.com>
2025-02-07 17:33:28 +0800

    Update triple-pendulum-swingup post:

--- content/post/triple-pendulum-swingup/notebooks/utils.py
+++ content/post/triple-pendulum-swingup/notebooks/utils.py
@@ -6,6 +6,7 @@ def CartMultiPendulumSystem(m_cart=1, m1=1, l1=1, **kwargs):
         m1: mass of pendulum #1 in kg
         l1: length of pendulum #1 in kg
         m2, l2, ...
+        encoder_ticks_per_rev: the number of ticks of the rotary encoder per revolution, adds no encoder if the value is (default) inf
 
     Returns:
         Diagram: get_input_port(0) is the force to the cart
@@ -14,9 +15,15 @@ def CartMultiPendulumSystem(m_cart=1, m1=1, l1=1, **kwargs):
     """
     from pydrake.all import (
         AddMultibodyPlantSceneGraph,
+        Demultiplexer,
         DiagramBuilder,
+        Multiplexer,
         Parser,
+        RotaryEncoders,
     )
+
+    encoder_ticks_per_rev = kwargs.pop('encoder_ticks_per_rev', float('inf'))
+    assert(encoder_ticks_per_rev > 0)
 
     kwargs['m1'] = m1
     kwargs['l1'] = l1
@@ -27,14 +34,32 @@ def CartMultiPendulumSystem(m_cart=1, m1=1, l1=1, **kwargs):
     Parser(plant, scene_graph).AddModelsFromString(sdf_string, "sdf")
     plant.Finalize()
 
-    builder.ExportInput(plant.GetInputPort("cart_multi_pendulum_actuation"), "f_cart")
+    demux = builder.AddSystem(Demultiplexer(
+        [1, num_pendulums, 1, num_pendulums]))
+    builder.Connect(plant.GetOutputPort("cart_multi_pendulum_state"), demux.get_input_port(0))
+
+    mux = builder.AddSystem(Multiplexer([1, num_pendulums]))
+    if encoder_ticks_per_rev == float('inf'):
+        builder.Connect(demux.get_output_port(0), mux.get_input_port(0))
+        builder.Connect(demux.get_output_port(1), mux.get_input_port(1))
+    else:
+        builder.Connect(demux.get_output_port(0), mux.get_input_port(0))
+        encoder = builder.AddSystem(RotaryEncoders([encoder_ticks_per_rev] * num_pendulums))
+        builder.Connect(demux.get_output_port(1), encoder.get_input_port())
+        builder.Connect(encoder.get_output_port(), mux.get_input_port(1))
+
+    builder.ExportInput(plant.GetInputPort(
+        "cart_multi_pendulum_actuation"), "f_cart")
     builder.ExportOutput(plant.GetOutputPort("cart_multi_pendulum_state"), "x")
+    builder.ExportOutput(mux.get_output_port(), "y")
     builder.ExportOutput(scene_graph.get_query_output_port(), "query")
 
     cart_multi_pendulum = builder.Build()
 
-    numerals = {1:'Single', 2:'Double', 3:'Triple', 4:'Quadruple', 5: 'Quintuple', 6: 'Sextuple'}
-    cart_multi_pendulum.set_name(f"Cart{numerals[num_pendulums]}Pendulum" if num_pendulums in numerals else f"Cart{num_pendulums}Pendulum")
+    numerals = {1: 'Single', 2: 'Double', 3: 'Triple',
+                4: 'Quadruple', 5: 'Quintuple', 6: 'Sextuple'}
+    cart_multi_pendulum.set_name(
+        f"Cart{numerals[num_pendulums]}Pendulum" if num_pendulums in numerals else f"Cart{num_pendulums}Pendulum")
     cart_multi_pendulum.num_pendulums = num_pendulums
 
     return cart_multi_pendulum
@@ -103,18 +128,20 @@ def _CartMultiPendulumSdf(m_cart, **kwargs):
     """
 
     # colormap interpolating red -> green -> blue
-    cmap = LinearSegmentedColormap.from_list("red_green_blue", [(1,0,0), (0,1,0), (0,0,1)])
+    cmap = LinearSegmentedColormap.from_list(
+        "red_green_blue", [(1, 0, 0), (0, 1, 0), (0, 0, 1)])
 
     # parent_link and z for each joint, update each iteration
     parent_link = 'cart'
     y = -0.17
     z = 0.0
-    for i in range(1,num_pendulums+1):
+    for i in range(1, num_pendulums+1):
         mi = kwargs[f'm{i}']
         li = kwargs[f'l{i}']
         rod_radius = 0.02
 
-        color = cmap((i-1) / (num_pendulums-1)) if num_pendulums > 1 else cmap(1)
+        color = cmap((i-1) / (num_pendulums-1)
+                     ) if num_pendulums > 1 else cmap(1)
 
         sdf_string += f"""
         <link name="pendulum{i}">
